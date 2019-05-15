c33ca04ab9f666d0c746bd1b60bda5d20d299810
bitfishxyz@gmail.com <bitfishxyz@gmail.com>
2019-05-15 11:20:33 +0800

    init

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,7 +2,10 @@
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
+    // 访问首页
     cy.visit('/')
+
+    // 检查页面包好指定内容
     cy.contains('h1', 'Welcome to Your Vue.js App')
   })
 })
